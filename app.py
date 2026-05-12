cdedf353f0392db917c4a041ad6c25337c7222b9
kontepan90-crypto <kontepan90@gmail.com>
2026-05-12 18:19:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -77,6 +77,5 @@ def admin_bookings():
 
 if __name__ == "__main__":
     init_db()
-    if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
